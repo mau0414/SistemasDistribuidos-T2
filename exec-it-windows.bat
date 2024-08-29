57bd021e0cad92182efc45d15bc7c66bf3e0a0f1
Leo Rossi <lr7leorossi07@gmail.com>
2024-08-29 14:22:06 -0300

    File bat para executar no windows

---
+++ exec-it-windows.bat
@@ -0,0 +1,7 @@
+@echo off
+
+start cmd /k "python supplier.py"
+start cmd /k "python product_stock.py"
+start cmd /k "python warehouse.py"
+start cmd /k "python line.py 0"
+start cmd /k "python factory.py empurrada 5 48"
